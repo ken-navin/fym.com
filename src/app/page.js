928c64f12e0decc6f9f2bc926dd99014c9e6f9e9
ken-navin <naveen2701m7@gmail.com>
2024-11-13 20:49:26 +0530

    test push

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Test from "./components/Test";
 
 export default function Home() {
   return (
     <> 
       <Navbar /> 
       <HeroSection/>
+      <Test/>
     </>
   );
 }
