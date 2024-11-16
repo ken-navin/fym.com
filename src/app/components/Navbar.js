f761c039075a32dcb2cd062e2e3fd26eb8d642fb
vighnesh <vighneshsu39@gmail.com>
2024-11-16 21:54:28 +0530

    Nav bar ui fix

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,18 +4,18 @@ import { AppBar, Toolbar, Typography, Box } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <AppBar position="static" color="transparent" elevation={0}>
-      <Toolbar sx={{ justifyContent: "space-between", backgroundColor: "#efeaea"}}>
+    <AppBar position="static" color="transparent" >
+      <Toolbar sx={{ justifyContent: "space-between", backgroundColor: "#ffffff",  }}>
         <Box sx={{ textAlign: "center",}}>
-          <Typography sx={{ color: "#D58D70",fontWeight: "bold", fontSize: "36px", fontFamily: "Readex Pro"}} >
+          <Typography sx={{ color: "#D58D70",fontWeight: "bold", fontSize: "36px", fontFamily: `"Readex Pro", sans-serif`}} >
             FYM.COM
           </Typography>
-          <Typography sx={{ fontSize: "14px", color: "#000000", fontFamily: "Readex Pro"}} >
+          <Typography sx={{ fontSize: "14px", color: "#000000", fontFamily: `"Readex Pro", sans-serif`, marginBottom: "10px"}} >
             Find your match
           </Typography>
         </Box>
   
-        <Typography sx={{ fontSize: "22px", color: "#000000", fontFamily: "Readex Pro"}} >
+        <Typography sx={{ fontSize: "22px", color: "#000000", fontFamily: `"Readex Pro", sans-serif` ,cursor: "pointer"}} >
           Help
         </Typography>
       </Toolbar>
