fe9d57f45037b63cf0a90673c419beec8c639885
ken-navin <naveen2701m7@gmail.com>
2024-11-16 20:08:48 +0530

    home page changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 
 import HeroSection from "./Components/HeroSection";
 import Navbar from "./components/Navbar";
-import Test from "./components/Test";
 
 export default function Home() {
   return (
     <> 
       <Navbar /> 
       <HeroSection/>
-      <Test/>
     </>
   );
 }
